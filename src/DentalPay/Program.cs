8f9041b1432577e66d7a70606fbda3558608b1d6
Djordje Radovic <120112255+djole994@users.noreply.github.com>
2026-01-10 10:42:55 +0100

    Update Program.cs

--- src/DentalPay/Program.cs
+++ src/DentalPay/Program.cs
@@ -1,3 +1,31 @@
-builder.Services.Configure<XmlExportOptions>(
-    builder.Configuration.GetSection("XmlExportOptions"));
+using DentalPay.Options;             
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StomatologijaContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.Configure<XmlExportOptions>(
+    builder.Configuration.GetSection(XmlExportOptions.SectionName));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
